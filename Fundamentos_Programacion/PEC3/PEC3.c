c4731705cf0de35acff6a7ed27f73360d333a905
Delgado Flores <pablo.delgado.flores@accenture.com>
2019-10-14 14:30:53 +0200

    Terminado el .c de la PEC3.

--- Fundamentos_Programacion/PEC3/PEC3.c
+++ Fundamentos_Programacion/PEC3/PEC3.c
@@ -17,9 +17,6 @@
 #define HOTEL2 2
 #define HOTEL3 3
 
-/*Definimos el tipo enumerado para los booleanos*/
-//typedef enum {FALSE,TRUE} boolean;
-
 /*Comenzamos la definicion del programa principal*/
 int main(int argc, char **argv)
 {
@@ -30,26 +27,19 @@ int main(int argc, char **argv)
 	int guests;
 	float priceWithDiscount[3];
 	float totalPricePerDay[3];
-	
-	
+	/*Comenzamos a pedir los precios y descuentos*/
 	printf("Introduce el precio por habitacion doble del primer hotel (%s): ",NAME_HOTEL1);
 	scanf("%d",&prices[0]);
 	printf("Introduce el precio por habitacion doble del segundo hotel (%s): ",NAME_HOTEL2);
 	scanf("%d",&prices[1]);
 	printf("Introduce el precio por habitacion doble del tercer hotel (%s): ",NAME_HOTEL3);
 	scanf("%d",&prices[2]);
-	
-	printf("Precios: %d,%d,%d\n",prices[0],prices[1],prices[2]);
-	
 	printf("Introduce el descuento del primer hotel: ");
 	scanf("%d",&discount[0]);
 	printf("Introduce el descuento del segundo hotel: ");
 	scanf("%d",&discount[1]);
 	printf("Introduce el descuento del tercer hotel: ");
 	scanf("%d",&discount[2]);
-	
-	printf("Descuentos: %d,%d,%d\n",prices[0],prices[1],prices[2]);
-	
 	if ((discount[0]>=0 && discount[0]<=MAX_DISCOUNT) && (discount[1]>=0 && discount[1]<=MAX_DISCOUNT) && (discount[2]>=0 && discount[2]<=MAX_DISCOUNT)){
 		printf("Introduce el numero de personas que iran al hotel: ");
 		scanf("%d",&guests);
@@ -67,13 +57,7 @@ int main(int argc, char **argv)
 			} else {
 				printf("El hotel mas barato es el %d, %s con un precio de %.2f\n",HOTEL3,NAME_HOTEL3,totalPricePerDay[2]);
 			}
-			
-		} else {
-			printf("Paco");
 		}
-		
-		
-		
 	} else {
 		printf("\n=================================================================================================================\n");
 		printf("ERROR:El descuento introducido es erroneo, debe ser una cantidad entre 0 y 50, ambos inclusive.\n");
